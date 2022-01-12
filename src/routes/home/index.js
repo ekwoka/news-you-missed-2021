ef0624a1e8af1c25c472b1d902478d4190066bf7
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-01-12 17:08:37 -0600

    :sparkles: Add Map

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,9 +1,11 @@
 import { h } from 'preact';
 import Hero from "../../components/hero";
+import Map from "../../components/map";
 
 const Home = () => (
 	<main className="text-emerald-900">
 		<Hero />
+		<Map />
 	</main>
 );
 
