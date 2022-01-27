ed8457b49bfa3fe897af1fee1aa60c320f521dbf
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-01-27 11:51:43 -0800

    Github.dev test

---
+++ src/components/preview/index.js
@@ -0,0 +1,12 @@
+
+
+export default function ArticlePreview(title, img) {
+
+
+    return (
+        <div>
+            <img src={img} alt={title} class='w-full object-cover'/>
+            <h1>{title}</h1>
+        </div>
+    )
+}
