b28c3d1520dcf3505d0b852ac1047df93aba6fa1
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-01-31 10:53:25 +0800

    Update index.js

--- src/components/preview/index.js
+++ src/components/preview/index.js
@@ -5,7 +5,7 @@ export default function ArticlePreview(title, img) {
 
     return (
         <div>
-            <img src={img} alt={title} class='w-full object-cover'/>
+            <img src={img} alt={title} class='w-full object-cover' />
             <h1>{title}</h1>
         </div>
     )
