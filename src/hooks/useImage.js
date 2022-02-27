4a5a030048eb5ec9e045c38f46dd00055114992c
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-02-27 16:28:55 +0800

    :memo: useImage comments

--- src/hooks/useImage.js
+++ src/hooks/useImage.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'preact/hooks';
 import { useInView } from 'react-intersection-observer';
 
-const cache = getStorageProxy('image-chache');
-const queue = [];
-let running = false;
-let errorStreak = 0;
+/* This whole thing is a bit hacky, as it's my first time using Preact, and of course, my first time making any custom hooks, or even using proxies.
 
-async function sleep(delay) {
-  return new Promise((resolve) => setTimeout(resolve, delay));
-}
+Goal: Dynamically fetch images for each country, store them in a cache so there is no need to do repeat fetches for the same country, and mirror the data to Local Storage.
+
+Challenges: 3 request per second to API, don't fetch uneeded image data.
+
+*/
+
+// Initialize Cache as Proxy to localStorage
+
+const cache = getStorageProxy('image-chache');
+
+// custom hook useImage, handles intersection observer to the image if lazyloading is neaded, and checks for image in cache, otherwise adds it to request queue
 
 export default function useImage(query, delay = false) {
   const { ref, inView } = useInView();
@@ -28,6 +33,8 @@ export default function useImage(query, delay = false) {
 
   return [image, ready, ref];
 }
+
+// Get image does a second check against the cache. This is to handle situations where an image is added to the queue multiple times and thus the first successful request can be used across all the slots in the queue
 
 async function getImage(q) {
   if (!q) return;
@@ -53,6 +60,20 @@ async function fetchImage(q) {
   return data;
 }
 
+/* Queue
+
+Stores pending image search requests.
+
+Processes the requests one at a time. Bing image search has a free limit of 3 searches per second. Processing them one at a time manages this well.
+
+Image requests that fail are added to the end of the stack, and a slight delay is instated. If 3 image requests fail in a row, the queue stops being processed until the page is refreshed.
+
+*/
+
+const queue = [];
+let running = false;
+let errorStreak = 0;
+
 function addToQueue(query, setImage, setReady) {
   queue.push({ query, setImage, setReady });
   if (!running) {
@@ -61,10 +82,12 @@ function addToQueue(query, setImage, setReady) {
   }
 }
 
+async function sleep(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
 async function processQueue() {
   while (queue.length > 0 && errorStreak < 3) {
-    console.log('Queue length:', queue.length);
-    console.log('Error streak:', errorStreak);
     const { query, setImage, setReady } = queue.shift();
     const image = await getImage(query);
     if (image.contentUrl) {
@@ -83,6 +106,8 @@ async function processQueue() {
   running = false;
   errorStreak = 0;
 }
+
+// The storage proxy for the cache. Simply checks for image data in localstorage and sets image data to localstorage. Localstorage is performant enough there isn't a need for keeping the cache in active memory in this use case.
 
 function getStorageProxy(prefix) {
   if (typeof window === 'undefined') return {};
